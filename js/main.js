11ee12e0c670868293bf9d4f2ff87520ca336737
QuieType <srulikpinson770@gmail.com>
2025-05-28 14:00:10 -0600

    code cleanup 2

--- js/main.js
+++ js/main.js
@@ -12,49 +12,6 @@ socket.addEventListener('open', () => {
     console.log('WebSocket connection established');
 });
 
-socket.addEventListener('message', (event) => {
-    const msg = JSON.parse(event.data);
-    if (msg.type === 'move') {
-        const target = [...document.querySelectorAll('.draggable')].find(el => el.dataset.cardId === msg.id);
-        if (target) {
-            target.style.left = msg.x + 'px';
-            target.style.top = msg.y + 'px';
-            target.setAttribute('data-rotation', msg.rotation);
-            target.style.transform = `translate(${-10 * msg.z}px, ${-10 * msg.z}px)`;
-            target.style.zIndex = ++currentZ;
-            const info = target.querySelector('.info');
-            info.style.transform = `rotate(${msg.rotation}deg)`;
-        }
-    }
-    else if (msg.type === 'delete') {
-        const target = [...document.querySelectorAll('.draggable')].find(el => el.dataset.cardId === msg.id);
-        target.remove();
-        target = null;
-    }
-    else if (msg.type === 'duplicate') {
-        const target = [...document.querySelectorAll('.draggable')].find(el => el.dataset.cardId === msg.srcid);
-        const clone = target.cloneNode(true);
-        const offset = 20;
-        clone.dataset.cardId = msg.id;
-        clone.style.left = (parseInt(target.style.left, 10) + offset) + 'px';
-        clone.style.top = (parseInt(target.style.top, 10) + offset) + 'px';
-        clone.style.zIndex = ++currentZ;
-        document.body.appendChild(clone);
-        
-        // Ensure cloned element has same events
-        clone.addEventListener('mousedown', (e) => {
-            dragged = clone;
-            offsetX = e.clientX - clone.offsetLeft;
-            offsetY = e.clientY - clone.offsetTop;
-            clone.style.zIndex = ++currentZ;
-            clone.style.cursor = 'grabbing';
-            clone.classList.add('dragging');
-        });
-        clone.addEventListener('mouseenter', () => target = clone);
-        clone.addEventListener('mouseleave', () => target = null);
-    }
-});
-
 fetch(csvUrl)
   .then(response => response.text())
   .then(csvData => {
